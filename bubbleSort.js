cf165a0375cb5ff4a473d74c531351d0255a2666
fivefivepumpkin <linxiaonan9387@gmail.com>
2022-11-19 16:28:38 +0800

    fix

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,17 +1,21 @@
 function bubbleSort(arr){
     if(arr.length <= 1) return arr
-    let len = arr.length
+    let len = arr.length, boo = false, lastindex = 0, newLen = arr.length - 1 
     for(let i = 0; i < len; i++){
-        for(let j = 0; j< len - 1- i; j++){
+        for(let j = 0; j< newLen; j++){
             if(arr[j]> arr[j+1]){
+                boo = true
                 let temp = arr[j+1]
                 arr[j+1] = arr[j]
                 arr[j] = temp
+                lastindex = j
             }
         }
+        newLen = lastindex
+        if(!boo) break
     }
     return arr
 }
 
-let arr = [5,3,2,4,1]
+let arr = [5,3,2,1,4]
 console.log(bubbleSort(arr))
