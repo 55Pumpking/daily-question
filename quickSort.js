10940e11fad75e375dfb611cef314a3de7fcbef9
fivefivepumpkin <linxiaonan9387@gmail.com>
2022-11-19 12:21:00 +0800

    quickSort

--- quickSort.js
+++ quickSort.js
@@ -1,3 +1,16 @@
 function quickSort(arr){
-
+    if(arr.length <= 1) return arr
+    let mid = arr.splice(Math.floor(arr.length/2) ,1)
+    let len = arr.length
+    let left = [], right =[]
+    for(let i = 0; i< len; i++){
+        if(arr[i] > mid){
+            right.push(arr[i])
+        } else {
+            left.push(arr[i])
+        }
+    }
+    return quickSort(left).concat(mid, quickSort(right))
 }
+let arr = [3,5,4,2,1]
+console.log(quickSort(arr))
